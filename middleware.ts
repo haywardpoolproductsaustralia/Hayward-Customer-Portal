9901c3b27e1070cc7748028ce3a2b3ab3a38cf01
haywardpoolproductsaustralia <haywardpoolproductsaustralia@gmail.com>
2026-07-21 11:26:06 +1000

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -3,6 +3,7 @@ import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 const isPublicRoute = createRouteMatcher([
      '/sign-in(.*)',
      '/api/au-orders-inbox/ingest',
+     '/api/au-orders-inbox/process',
    ]);
 
 export default clerkMiddleware(async (auth, req) => {
